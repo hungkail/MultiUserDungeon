d22bdac855c8edc533dc0bf809d1e48787ac3052
Hung Kai Liao <hungkail@bekeley.edu>
2019-05-19 05:44:45 -0700

    fix command

--- chatScreen.php
+++ chatScreen.php
@@ -1,8 +1,10 @@
 <?php
 session_start();
+/** refresh page every 2 second
+ *  in order to show current chat message received
+ */
 ?>
-/* refresh page every 2 second
-in order to show current chat message received */
+
 <meta http-equiv="refresh" content="2">
 <link type="text/css" rel="stylesheet" href="styles.css" />
 <html>
