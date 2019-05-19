82adf243540be910612fac39f0295823cb0c0e43
Hung Kai Liao <hungkail@bekeley.edu>
2019-05-19 04:38:28 -0700

    update database path again

--- system/config.php
+++ system/config.php
@@ -4,6 +4,5 @@
     $dungeonHeight = 3;
 
 //    $conn_string = "host=localhost port=5434 dbname=sample user=postgres password= options='--client_encoding=UTF8'";
-    $conn_string = getenv("postgres://ntwdxjcpspaicr:99c4a38b3be2035fc01c18780811f8613135e91f982f37b984c93520018c25bf@ec2-184-72-237-95.compute-1.amazonaws.com:5432/dauirbnp544olr
-");
+    $conn_string = getenv("DATABASE_URL");
 ?>
