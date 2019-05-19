c64a051dbd7a3aa43dc7af5db8fecb988ebce184
Hung Kai Liao <hungkail@bekeley.edu>
2019-05-19 04:25:39 -0700

    update database path

--- system/config.php
+++ system/config.php
@@ -4,5 +4,6 @@
     $dungeonHeight = 3;
 
 //    $conn_string = "host=localhost port=5434 dbname=sample user=postgres password= options='--client_encoding=UTF8'";
-    $conn_string = getenv("https://data.heroku.com/datastores/122d13b1-ee5a-4824-a49f-18c3a4c3731f#");
+    $conn_string = getenv("postgres://ntwdxjcpspaicr:99c4a38b3be2035fc01c18780811f8613135e91f982f37b984c93520018c25bf@ec2-184-72-237-95.compute-1.amazonaws.com:5432/dauirbnp544olr
+");
 ?>
